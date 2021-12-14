fd31b5cb9ef00ed2001e5bb41dc483b2fda3ec68
xanderalex <lx16hd@gmail.com>
2021-12-14 09:32:45 +0000

    changed to discord notis, webhook

--- main.py
+++ main.py
@@ -3,41 +3,35 @@ import config.config as config
 import time
 import requests
 import json
-from lxml import html
-from kthread import KThread
 from datetime import date
+import discord
+from discord import Webhook, RequestsWebhookAdapter
+from threading import Thread
+import asyncio
+
+updates = 0
+id = 0
+
+wclient = Webhook.from_url(config.webhookurl, adapter=RequestsWebhookAdapter())
+
+client = discord.Client()
 
 session = requests.Session()
 today = date.today()
 privatekey = config.pskey
-client = Client(privatekey)
+discordtoken = config.discordtoken
+#client = Client(privatekey)
 
-id = ""
-updates = 0
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-latitude = "35.040554"
-longitude = "-117.138306"
-maxradius = "251.3414"
+latitude = "34.026291"
+longitude = "-117.981627"
+maxradius = "140.25834"
 orderby = "time"
 date = today.strftime("%y-%m-%d")
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=" + latitude + "&longitude=" + longitude + "&maxradiuskm=" + maxradius + "&orderby=" + orderby + "&limit=1" + "&starttime=" + date
-
-def neweq(mag, msg, URL):
-    title = "Earthquake Alert!"
-    deviceid = "47512"
-    icon = "2"
-    sound = ""
-    vibration = "2"
-    URLTitle = "Open USGS report"
-    Time2Live = "0"
-    Priority = "2"
-    retry = "60"
-    expire = "600"
-    answer = "1"
-    image1 = ""
-    image2 = ""
-    image3 = ""
-    client.send_message(msg, title, deviceid, icon, sound, vibration, URL, URLTitle, Time2Live, Priority, retry, expire, answer, image1, image2, image3)
 
 while True:
     initdata = session.get(url)
@@ -55,7 +49,30 @@ while True:
         mag = data_['mag']
         title = data_['title']
         urll = data_['url']
-        neweq(round(mag, 1), title, urll)
+        #neweq(round(mag, 1), title, urll)
+        #client.get_channel(config.channelid).send("Earthquake! " + title)
+        wclient.send("Earthquake! " + title + "\n" + urll)
         print(str(mag) + ": " + title + ", ")
-        
     time.sleep(5)
+
+
+
+def neweq(mag, msg, URL):
+    title = "Earthquake Alert!"
+    deviceid = "47512"
+    icon = "2"
+    sound = ""
+    vibration = "2"
+    URLTitle = "Open USGS report"
+    Time2Live = "0"
+    if mag >= 3.5:
+        Priority = "2"
+    else:
+        Priority = "0"
+    retry = "60"
+    expire = "600"
+    answer = "1"
+    image1 = ""
+    image2 = ""
+    image3 = ""
+    client.send_message(msg, title, deviceid, icon, sound, vibration, URL, URLTitle, Time2Live, Priority, retry, expire, answer, image1, image2, image3)
