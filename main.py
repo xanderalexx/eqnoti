c8270827d8566174f069701d35a813480f070ba4
xanderalex <lx16hd@gmail.com>
2021-12-16 11:06:57 +0000

    changed to embeded message, added sevenseq support

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pushsafer import Client
 import config.config as config
 import time
 import requests
@@ -6,25 +5,16 @@ import json
 from datetime import date
 import discord
 from discord import Webhook, RequestsWebhookAdapter
-from threading import Thread
-import asyncio
+import sevenseq.sevenseq as sevenseq
+import sys
 
 updates = 0
 id = 0
 
 wclient = Webhook.from_url(config.webhookurl, adapter=RequestsWebhookAdapter())
 
-client = discord.Client()
-
 session = requests.Session()
 today = date.today()
-privatekey = config.pskey
-discordtoken = config.discordtoken
-#client = Client(privatekey)
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
 latitude = "34.026291"
 longitude = "-117.981627"
@@ -33,6 +23,35 @@ orderby = "time"
 date = today.strftime("%y-%m-%d")
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=" + latitude + "&longitude=" + longitude + "&maxradiuskm=" + maxradius + "&orderby=" + orderby + "&limit=1" + "&starttime=" + date
 
+def convertplace(x):
+    hmm = "ab"
+    hmm = list(hmm)
+    hmm[0] = x[0]
+    if x[1] == "k":
+        hmm[1] = ""
+        x = x[3:len(x)]
+    else:
+        hmm[1] = x[1]
+        x = x[4:len(x)]
+    km = int(str(hmm[0] + hmm[1]))
+    return str(round((km / 1.609), 2)) + "mi" + x
+
+def returnembed(data_, geo):
+    mag = data_['mag']
+    title = data_['title']
+    urll = data_['url']
+    place = data_['place']
+    long = geo['coordinates'][0]
+    lat = geo['coordinates'][1]
+    geourl = "https://www.google.com/maps/search/?api=1&query=" + str(lat) + "," + str(long)
+    
+    embeded=discord.Embed(title="https://earthquake.usgs.gov/", url=urll, color=discord.Color.blue())
+    embeded.set_author(name="eqnoti", icon_url="https://cdn.discordapp.com/avatars/919764620003135538/0cf2d9fbf4a1f3f51da64487c08be936.webp")
+    embeded.set_thumbnail(url="https://www.wavy.com/wp-content/uploads/sites/3/2020/10/USGS_logo_green_SQUARE.png")
+    embeded.add_field(name = "**What:** ", value = "A " + str(round(mag, 1)) + " magnitude earthquake occurred " + convertplace(place))
+    embeded.add_field(name = "**Where:**", value = geourl)
+    return embeded
+    
 while True:
     initdata = session.get(url)
     data = json.loads(initdata.text)
@@ -40,6 +59,7 @@ while True:
     curtime = data['features'][0]['properties']['time']
     if curtime == id:
         updates = updates + 1
+        #sevenseq.setnum(updates)
         if updates <= 1:
             print(str(updates) + " update since last quake")
         else:
@@ -49,30 +69,12 @@ while True:
         mag = data_['mag']
         title = data_['title']
         urll = data_['url']
+        place = data_['place']
+        embeded = returnembed(data_, data['features'][0]['geometry'])
+        wclient.send(embed=embeded)
         #neweq(round(mag, 1), title, urll)
         #client.get_channel(config.channelid).send("Earthquake! " + title)
-        wclient.send("Earthquake! " + title + "\n" + urll)
-        print(str(mag) + ": " + title + ", ")
+        sevenseq.setnum(round(mag, 1), 2)
+        print(str(mag) + ": " + "A " + str(round(mag, 1)) + " magnitude earthquake occurred " + convertplace(place))
+        updates = 0
     time.sleep(5)
-
-
-
-def neweq(mag, msg, URL):
-    title = "Earthquake Alert!"
-    deviceid = "47512"
-    icon = "2"
-    sound = ""
-    vibration = "2"
-    URLTitle = "Open USGS report"
-    Time2Live = "0"
-    if mag >= 3.5:
-        Priority = "2"
-    else:
-        Priority = "0"
-    retry = "60"
-    expire = "600"
-    answer = "1"
-    image1 = ""
-    image2 = ""
-    image3 = ""
-    client.send_message(msg, title, deviceid, icon, sound, vibration, URL, URLTitle, Time2Live, Priority, retry, expire, answer, image1, image2, image3)
